5cdbba058724300f4456ecc070f6fc899065d8eb
Car27VeP <carlos.pazaran@outlook.com>
2024-05-14 22:09:21 -0600

    cambio app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ scatter_button = st.checkbox('Constuir dispersión')
 
 car_data['manufacturer'] = [model.split()[0] for model in car_data['model']]
 
-st.header("Aplicación web sobre las ventas de vehículos de Estados Unidos")
+st.header("Aplicación web sobre las ventas de vehículos de Estados Unidos.")
         
 if hist_button: # al hacer clic en el botón
 # escribir un mensaje
-    st.header('Histograma') 
+    st.header('Histograma.') 
 
-    st.write('Creación de histogramas para el conjunto de datos de anuncios de venta de coches') 
+    st.write('Creación de histogramas para el conjunto de datos de anuncios de venta de coches.') 
     
     fig = px.histogram(car_data, x="odometer",
                            title='Distribución del kilometraje de los vehículos.')
@@ -22,17 +22,17 @@ if hist_button: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
     
-    fig1 = px.histogram(car_data, x="manufacturer",color='transmission',title='Fabricantes con mayor registro')
+    fig1 = px.histogram(car_data, x="manufacturer",color='transmission',title='Fabricantes con mayor registro.')
     
     st.plotly_chart(fig1, use_container_width=True)
     
 if scatter_button:
     
-    st.header('Dispersión') 
+    st.header('Dispersión.') 
     
-    st.write('Creación de gráficas de dispersion de vehículos de Estados Unidos')
+    st.write('Creación de gráficas de dispersión de vehículos de Estados Unidos.')
     
-    fig = px.scatter(car_data, x="odometer", y="price", title='Precio de vehículos con respecto a su kilometraje',color='condition')
+    fig = px.scatter(car_data, x="odometer", y="price", title='Precio de vehículos con respecto a su kilometraje.',color='condition')
     
     st.plotly_chart(fig, use_container_width=True)
     
@@ -40,6 +40,6 @@ if scatter_button:
            x='model_year',
            y='price',
            color='model_year',
-           title='Precio de los vehículos con respecto al año de fabricación')
+           title='Precio de los vehículos con respecto al año de fabricación.')
     
     st.plotly_chart(fig1, use_container_width=True)
